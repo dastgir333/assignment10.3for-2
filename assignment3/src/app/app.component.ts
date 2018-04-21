e6341e377e419acce174e1923eb6d59e6e0c9395
dastgir333 <dastgir333@gmail.com>
2018-04-21 17:55:09 +0530

    Add files via upload

---
+++ assignment3/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { ListService } from "app/service/list.service";
+import { DropDownService } from "app/service/drop-down.service";
+import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
+
+@Component({
+  selector: 'app-component',
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ListService]
+})
+export class RootComponent {
+
+  arrayList: any[] = [];
+  genderArray: any[] = [];
+
+  userForm: FormGroup;
+
+  constructor(private _listService: ListService, private _dropDownService: DropDownService, private fb: FormBuilder) { }
+
+  ngOnInit() {
+
+    this.userForm = this.fb.group({
+      'name': ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z]*$')])],
+      'lastName': ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z]*$')])],
+      'age': ['', Validators.compose([Validators.required])],
+      'genderType': [null, Validators.required]
+    });
+    this.genderArray = this._dropDownService.getDropDown();
+  };
+
+
+  submit(values) {
+    let model = {
+      name: values.name,
+      lastName: values.lastName,
+      age: values.age,
+      gender: values.genderType
+    };
+    this._listService.addList(model);
+    this.arrayList = this._listService.getList();
+  };
+
+}
